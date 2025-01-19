d12b7023d1aeafc9f33d3db4f26f2aaedb4bc0ab
Md. Arif Hassan <aarifhsn@gmail.com>
2025-01-20 00:37:42 +0600

    resolved the git issues

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,39 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('/browse-jobs', function () {
+    return view('job-lists');
+})->name('job-lists');
+
+Route::get('/job-categories', function () {
+    return view('job-categories');
+})->name('job-categories');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/manage-jobs', function () {
+    return view('manage-jobs');
+})->name('manage-jobs');
+
+Route::get('/manage-jobs-post', function () {
+    return view('manage-jobs-post');
+})->name('manage-jobs-post');
+
+Route::get('/job-details', function () {
+    return view('job-details');
+})->name('job-details');
+
+Route::get('/profile', function () {
+    return view('profile');
+})->name('profile');
+
+Route::get('/logout', function () {
+    return view('logout');
+})->name('logout');
+
+
